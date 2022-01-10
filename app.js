5982ca565da37f6db79cb7b3bdf73a913f82432f
Nicholas Gersbach <nicholas.j.gersbach@gmail.com>
2022-01-10 13:35:31 -0700

    validation

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ document.querySelector(".menu-btn").addEventListener("click", function () {
 });
 
 //Email Validate
+const emailInput = document.getElementById("email");
+const emailError = document.querySelector(".email-error-msg");
+const emailSubmit = document.getElementById("contact-form-submit");
+
+emailSubmit.addEventListener("click", validateEmail);
+
+function validateEmail(e) {
+  if (emailInput.value === "") {
+    emailError.style.visibility = "visible";
+  }
+  // e.preventDefault();
+}
